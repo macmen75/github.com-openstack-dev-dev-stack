ae7f0c32b916bfc5c112fe99b4cacb1f69b1b3f8
macmen75 <macmen75@yahoo.com>
2026-02-24 10:40:48 +0800

    Define outputs for address objects in outputs.tf
    Add outputs for created address objects and their details.

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+output "address_objects_created" {
+  description = "List of created address objects"
+  value       = [for obj in fortimanager_object_address.address : obj.name]
+}
+
+output "address_objects_details" {
+  description = "Details of all created address objects"
+  value = {
+    for name, obj in fortimanager_object_address.address : name => {
+      subnet  = obj.subnet
+      type    = obj.type
+      color   = obj.color
+      comment = obj.comment
+    }
+  }
+}
