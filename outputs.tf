887b3e2d0db780070324618f9563a4d233183716
macmen75 <macmen75@yahoo.com>
2026-02-24 11:57:43 +0800

    Update output references to firewall address objects

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "address_objects_created" {
   description = "List of created address objects"
-  value       = [for obj in fortimanager_object_address.address : obj.name]
+  value       = [for obj in fortimanager_object_firewall_address.address : obj.name]
 }
 
 output "address_objects_details" {
   description = "Details of all created address objects"
   value = {
-    for name, obj in fortimanager_object_address.address : name => {
+    for name, obj in fortimanager_object_firewall_address.address : name => {
       subnet  = obj.subnet
       type    = obj.type
       color   = obj.color
