8ae7b37cdbbed05586a7a3f71d29f2b980154e56
macmen75 <macmen75@yahoo.com>
2026-02-24 11:40:24 +0800

    Update FortiManager provider source in Terraform

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5.0"
   required_providers {
     fortimanager = {
-      source  = "Fortinet/fortimanager"
+      source  = "fortinetdev/fortimanager"
       version = "~> 2.28"
     }
   }
