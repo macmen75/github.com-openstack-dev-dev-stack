9db4d1d27bc37682897bb2d308610f14913539c0
macmen75 <macmen75@yahoo.com>
2026-02-24 10:38:37 +0800

    Add FortiManager provider configuration

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    fortimanager = {
+      source  = "Fortinet/fortimanager"
+      version = "~> 2.28"
+    }
+  }
+}
+
+provider "fortimanager" {
+  hostname = var.fmg_host
+  token    = var.fmg_api_token
+  insecure = var.fmg_insecure
+}
